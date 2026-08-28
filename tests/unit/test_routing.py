4962338a795bdc55c62cc2d84e68ee5746be7489
AmSickOfCoding <alnimersaif@gmail.com>
2026-08-28 20:21:49 +0300

    test(routing): add unit test suite covering pathfinding and graph disruption scenarios

---
+++ tests/unit/test_routing.py
@@ -0,0 +1,10 @@
+"""Unit test entrypoint for RESQ spatial routing package."""
+
+from tests.test_routing import (
+    TestDataStructures as _TestDataStructures,
+    TestDynamicDisruptions as _TestDynamicDisruptions,
+    TestCostModeling as _TestCostModeling,
+    TestPathfinder as _TestPathfinder,
+    TestFacilitySelection as _TestFacilitySelection,
+    TestGenerator as _TestGenerator,
+)
